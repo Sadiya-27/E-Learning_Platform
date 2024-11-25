d1355ddcd59d0f0ae80782798ee648a3fc206afe
Sadiya Fatima Khwaja <104360019+Sadiya-27@users.noreply.github.com>
2024-11-25 19:20:47 +0530

    db.js

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,1 @@
-export const connectionStr = "mongodb+srv://khwajasadiya:rjIzenW9zkw80XFc@elearning.wvmk5.mongodb.net/db?retryWrites=true&w=majority&appName=elearning"
+export const connectionStr = "mongodb+srv://<username>:<password>@elearning.wvmk5.mongodb.net/db?retryWrites=true&w=majority&appName=elearning"
