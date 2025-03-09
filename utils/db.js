d0652b5be8873f04ba96a02136441fc4f4ca4579
Sadiya Fatima Khwaja <104360019+Sadiya-27@users.noreply.github.com>
2025-03-10 01:38:26 +0530

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,1 @@
-export const connectionStr = "export const connectionStr = "mongodb+srv://khwajasadiya:rjIzenW9zkw80XFc@elearning.wvmk5.mongodb.net/db?retryWrites=true&w=majority&appName=elearning"
-"
+export const connectionStr = "mongodb+srv://khwajasadiya:rjIzenW9zkw80XFc@elearning.wvmk5.mongodb.net/db?retryWrites=true&w=majority&appName=elearning"
